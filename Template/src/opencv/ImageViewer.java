e111e8ed810564174eca0c704ca8d95a5787cff5
Paulo andrade <source.compu@gmail.com>
2018-03-28 13:55:28 -0600

    Se añadio un proyecto template para los proyectos.

---
+++ Template/src/opencv/ImageViewer.java
@@ -0,0 +1,190 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opencv;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import org.opencv.core.Mat;
+
+/**
+ *
+ * @author Paulo Andrade
+ */
+public class ImageViewer extends JFrame implements ActionListener
+{
+    JPanel mainPanel, camPanel, optionPanel;
+    JLabel lblImg;
+    JButton btnStop, btnStart, btnClose;
+    String title;
+    private int width, height;
+    private boolean ctrCam, ctrWindow;
+    
+    /**
+     * Constructor
+     * 
+     * @param title Titulo para la ventana
+     */
+    public ImageViewer(String title)
+    {
+        this.title = title; // Titulo del JFrame
+        width = 800; // Ancho del JFrame (default 660)
+        height = 600; // Alto del JFrame (default 500)
+        ctrCam = false; // Control para activar/deactivar la camara
+        ctrWindow = false; // Control para cerrar la ventana
+        
+        // Inicializamos los componentes
+        initComponents();
+    }
+    
+    /**
+     * Metodo getter ctrCam
+     * 
+     * @return Devuelve el estado de la camara
+     */
+    public boolean getCtrCam()
+    {
+        return ctrCam;
+    }
+    
+    /**
+     * Metodo getter ctrWindow
+     * 
+     * @return Devuelve el estado de la ventana
+     */
+    public boolean getCtrWindow()
+    {
+        return ctrWindow;
+    }
+    
+    /**
+     * Inicializa los componentes del JFrame
+     */
+    private void initComponents()
+    {
+        setTitle(title);
+        setSize(width, height + 20);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        
+        // Componentes
+        mainPanel = new JPanel();
+        mainPanel.setBounds(0, 0, width, height);
+        mainPanel.setBackground(Color.WHITE);
+        mainPanel.setLayout(null);
+        
+        lblImg = new JLabel();
+        lblImg.setBounds(10, 100, 640, 480);
+        
+        camPanel = new JPanel();
+        camPanel.setBounds(10, 10, 640, 80);
+        camPanel.setBackground(Color.WHITE);
+        camPanel.setBorder(BorderFactory.createTitledBorder("Control de camara"));
+        
+        btnStop = new JButton("Stop");
+        btnStop.setBounds(10, 20, 150, 20);
+        btnStop.setEnabled(false);
+        
+        btnStart = new JButton("Start");
+        btnStart.setBounds(170, 20, 150, 20);
+        
+        btnClose = new JButton("Close");
+        btnClose.setBounds(330, 20, 150, 20);
+        
+        optionPanel = new JPanel();
+        optionPanel.setBounds(650, 10, 140, 580);
+        optionPanel.setBackground(Color.WHITE);
+        optionPanel.setBorder(BorderFactory.createTitledBorder("Opciones"));
+        optionPanel.setLayout(null);
+        
+        // Añadimos componentes
+        add(mainPanel);
+        mainPanel.add(lblImg);
+        mainPanel.add(camPanel);
+        camPanel.add(btnStop);
+        camPanel.add(btnStart);
+        camPanel.add(btnClose);
+        mainPanel.add(optionPanel);
+        
+        // Eventos
+        btnStop.addActionListener(this);
+        btnStart.addActionListener(this);
+        btnClose.addActionListener(this);
+        
+        // Mostramos JFrame
+        setVisible(true);
+    }
+    
+    /**
+     * Redimenciona el ancho y alto del JFrame
+     * 
+     * @param width Ancho del JFrame
+     * @param height Alto del JFrame
+     */
+    public void set(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    
+    /**
+     * Muestra un frame en pantalla reconociendo las caras
+     * 
+     * @param m matriz a mostrar en pantalla
+     */
+    public void show(Mat m)
+    {   
+        // Convertimos la matriz en imagen
+        Image img = ImageProcessor.toBufferedImage(m);
+        
+        // Mostramos la imagen
+        lblImg.setIcon(new ImageIcon(img));
+    }
+    
+    /**
+     * Cerramos la ventana
+     */
+    public void close()
+    {
+        // Destruimos la ventana
+        dispose();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Verificamos el boton pulsado
+        if(e.getSource().equals(btnStop)){
+            // Desactivamos el boton
+            btnStop.setEnabled(false);
+            btnStart.setEnabled(true);
+            btnClose.setEnabled(true);
+            
+            // Actualizamos el control
+            ctrCam = !ctrCam;
+        } else if(e.getSource().equals(btnStart)){
+            // Desactivamos el boton
+            btnStart.setEnabled(false);
+            btnStop.setEnabled(true);
+            btnClose.setEnabled(false);
+            
+            // Actualizamos el control
+            ctrCam = !ctrCam;
+        } else if(e.getSource().equals(btnClose)){
+            ctrWindow = !ctrWindow;
+        }
+    }
+}
---
+++ Template/src/opencv/Main.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opencv;
+
+// Importamos las librerias
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.Videoio;
+
+/**
+ *
+ * @author Paulo Andrade
+ */
+public class Main
+{   
+    // Cargamos las librerias dinamicas de OpenCV
+    static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+    
+    private static ScheduledExecutorService timer; // 
+    private static VideoCapture cam; // Dispositivo
+    private static int ID_DEVICE = 0; // ID de dispositivo
+    private static int DELAY = 33; // Tiempo en milisegundos
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // creamos la matriz para los frame
+        Mat frame = new Mat();
+        // accedemos al dispositivo
+        cam = new VideoCapture();
+        // configuramos el ancho y alto
+        cam.set(Videoio.CAP_PROP_FRAME_WIDTH, 640);
+        cam.set(Videoio.CAP_PROP_FRAME_HEIGHT, 480);
+        // Creamos la ventana
+        ImageViewer view = new ImageViewer("Recocimiento facial basico");
+        // actualizamos la configuracion
+        view.set(640, 480);
+        
+        // Abrimos la camara
+        startCamera();
+            
+        // Creamos un hilo para trabajar, tomando un frame segun el delay
+        Runnable frameGrabber = new Runnable() {
+            @Override
+            public void run() {
+                // Verificamos que accedemos al dispositivo
+                if(cam.isOpened()){
+                    // verificamos el estado de la ventana
+                    if(view.getCtrWindow()){
+                        // Detenemos la camara
+                        stopCamera();
+                        // Cerramos la ventana
+                        view.close();
+                    }
+                    // Verificamos el estado de la camara
+                    if(view.getCtrCam()){
+                        // Inicializamos el tiempo transcurrido
+                        long startTime = System.currentTimeMillis();
+
+                        // Capturamos el frame
+                        cam.read(frame);
+
+                        // Detectamos los rostros
+                        view.show(frame);
+
+                        // Obtenemos el tiempo de proceso
+                        long processTime = System.currentTimeMillis() - startTime;
+                        System.out.println("Time process: "+processTime+" ms");
+                    }
+                } else {
+                    // Detenemos la camara
+                    stopCamera();
+                    // Mostramos error de carga
+                    System.out.println("Error al inicializar el dispositivo");
+                }
+            }
+        };
+
+        timer = Executors.newSingleThreadScheduledExecutor();
+        timer.scheduleAtFixedRate(frameGrabber, 0, DELAY, TimeUnit.MILLISECONDS);    
+    }
+    
+    /**
+     * Detiene la camara
+     */
+    private static void stopCamera(){
+        // verificamos si el tiempo esta corriendo
+        if (timer!=null && !timer.isShutdown()){
+            try{
+                // Detenemos el tiempo
+                timer.shutdown();
+                timer.awaitTermination(33, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                // log any exception
+                System.err.println("Exception in stopping the frame capture, trying to release the camera now... " + e);
+            }
+	}
+		
+	if (cam.isOpened()){
+            // release the camera
+            cam.release();
+	}
+    }
+    
+    /**
+     * Inicia la camara
+     */
+    private static void startCamera()
+    {
+        // Abrimos el dispositivo
+        cam.open(ID_DEVICE);
+    }
+}
